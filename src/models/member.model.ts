fcbdd5fbef8799b017e3b3f607312d1cd8fd04e6
Nick-Lemy <n.kayiranga@alustudent.com>
2026-04-30 18:28:29 +0200

    feat: remove Member interface from models

--- src/models/member.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Member {
-  id: string;
-  name: string;
-  email: string;
-  githubUsername: string;
-  orgName: string;
-  joinReason: string;
-  codingLevel: "beginner" | "intermediate" | "advanced";
-}
