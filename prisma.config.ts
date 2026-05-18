ddf76139ac5a388b68977c5b763ac2f41dc16151
ub-victor <victoireushindi371@gmail.com>
2026-05-18 17:31:59 +0200

    fix the conf

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+
+// npm install --save-dev prisma dotenv
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
+  },
+});
+ 
