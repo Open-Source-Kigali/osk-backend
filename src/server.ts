b15ebd979bef5534e13e725394824633a78055f8
ME Cyusaa <hssngotvbzz@gmail.com>
2026-05-19 15:47:26 +0200

    fix #30

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,6 @@ if (!env.adminApiKey) {
     "WARNING: ADMIN_API_KEY is not set. All admin endpoints will return 500.",
   );
 }
-// if (!env.adminApiKey) {
-//   console.error('FATAL: ADMIN_API_KEY is not set. Refusing to start.')
-//   process.exit(1)
-// }
 app.listen(env.port, () => {
   console.log(`Server running on http://localhost:${env.port}`);
 });
