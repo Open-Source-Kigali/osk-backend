cae491f0e1ea4274aa328f186bcfbe7c1504c2de
Josephat-S <sangwajozaphat@gmail.com>
2026-05-19 10:22:51 +0200

    feat: add contributors endpoints and cache

---
+++ src/controllers/contributors.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { readContributors, refreshContributors } from "../services/contributors.service";
+import response from "../utils/response";
+
+export async function getContributors(req: Request, res: Response) {
+  try {
+    const contributors = await readContributors();
+    return response.success(res, contributors);
+  } catch (error) {
+    return response.failure(res, "Failed to read contributors", 500);
+  }
+}
+
+export async function refresh(req: Request, res: Response) {
+  try {
+    const result = await refreshContributors();
+    return response.success(res, result, "Contributors refreshed successfully");
+  } catch (error) {
+    return response.failure(res, "Failed to refresh contributors", 500);
+  }
+}
