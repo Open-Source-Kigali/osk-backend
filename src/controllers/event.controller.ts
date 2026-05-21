490ae542bc2192d1ca39b0f6c87e732248bb5371
mug1sha <godson.mugisha2015@gmail.com>
2026-05-22 00:15:01 +0200

    feat: support featured query parameter for events and projects

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -63,9 +63,10 @@ function buildEventData(
   return data;
 }
 
-async function findAllEvents(_req: Request, res: Response, next: NextFunction) {
+async function findAllEvents(req: Request, res: Response, next: NextFunction) {
   try {
-    const allEvents = await eventService.findAllEvents();
+    const featured = req.query.featured === "true" ? true : undefined;
+    const allEvents = await eventService.findAllEvents(featured);
     response.success(res, allEvents, 200, "Events retrieved successfully");
   } catch (err) {
     next(err);
