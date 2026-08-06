1cc0040d068cb1bb3dd48b56599e9f51cbb07822
Emmanuel A. <alabiemmanuel2937@gmail.com>
2026-08-06 08:00:52 +0200

    remove unused contributor.routes.ts

--- src/routes/contributor.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import contributorController from "../controllers/contributor.controller";
-import { publicRateLimit } from "../middlewares/rate-limit.middleware";
-
-const router = Router();
-
-router.get("/", publicRateLimit, contributorController.findAllContributors);
-
-export default router;
